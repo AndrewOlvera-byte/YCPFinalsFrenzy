6856c47c18bf2ef27e1fe525e0041c0dc41a88b8
rthan1 <ethanwnelson@gmail.com>
2025-03-17 20:02:36 -0400

    Item and Inventory

---
+++ src/models/Item.java
@@ -0,0 +1,45 @@
+package models;
+public class Item {
+	private int value, weight;
+	private String name;
+	private String[] components;
+	
+	public Item(int value, int weight, String name, String[] components) {
+		this.weight = weight;
+		this.value = value;
+		this.name = name;
+		this.components = components;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	} 
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String[] getComponents() {
+        return components.clone();
+    }
+	
+	public void setComponents(String[] components) {
+		this.components = components;
+	}
+}
