f03ece2d2f4da912844d033f5f54e1b1df329d42
rthan1 <ethanwnelson@gmail.com>
2025-05-08 16:46:25 -0400

    Fixed damage taken
    Prints out the correct amount of damage taken from an aggressive npc.

--- src/models/CombatManager.java
+++ src/models/CombatManager.java
@@ -49,7 +49,7 @@ public class CombatManager {
         }
         else {
             currentRoom.setCharacterHealth(characterNum, newHealth);
-            charAttackPlayer(0, characterNum, aggressive);
+            int totalDmgTaken = charAttackPlayer(0, characterNum, aggressive);
 
             // Format attack damage string
             String dmgString = (attackDmg % 1 == 0)
@@ -60,9 +60,8 @@ public class CombatManager {
                 return "\n<b>You Died!</b>";
             }
             else if (aggressive) {
-                int counterDmg = currentRoom.getCharacterAttackDmg(characterNum, 0);
                 return "<b>\n" + currentRoom.getCharacterName(characterNum) + " has taken " + dmgString + " damage.</b>" +
-                       "<b>\n" + currentRoom.getCharacterName(characterNum) + " hit back for " + counterDmg + " damage.</b>";
+                       "<b>\n" + currentRoom.getCharacterName(characterNum) + " hit back for " + totalDmgTaken + " damage.</b>";
             }
             else {
                 return "\n" + "<b>" + currentRoom.getCharacterName(characterNum) + " has taken " + dmgString + " damage.</b>";
@@ -71,17 +70,18 @@ public class CombatManager {
     }
     
     // Method for the character characterNum attacking the player with itemNum
-    public void charAttackPlayer(int itemNum, int characterNum, boolean aggressive) {
+    public int charAttackPlayer(int itemNum, int characterNum, boolean aggressive) {
         Room currentRoom = engine.getRooms().get(engine.getCurrentRoomNum());
         Character character = currentRoom.getCharacter(characterNum);
 
         // 1) Only attack if the character is aggressive
-        if (!aggressive) return;
+        if (!aggressive) 
+        	return 0;
 
         // 2) Ensure they have a weapon to hit you with
         if (character.getInventorySize() <= itemNum) {
             engine.appendMessage("\n" + character.getName() + " has no weapon to attack with.");
-            return;
+            return 0;
         }
 
         // 3) Base (raw) damage from the NPC
@@ -100,6 +100,7 @@ public class CombatManager {
         // 8) Finally, subtract from the player’s HP
         int playerHp = engine.getPlayer().getHp();
         engine.getPlayer().setHp(playerHp - (int)Math.round(totalDmg));
+        return (int)Math.round(totalDmg);
     }
 
 }
